f5a1a56e8d6f30238108f037b844e338d6906e92
JP Lynch <JPLynch35@gmail.com>
2018-11-04 08:19:06 -0700

    Refactor get word spelling actions to seperate private method

--- app/serializers/anagram_key_serializer.rb
+++ app/serializers/anagram_key_serializer.rb
@@ -2,10 +2,16 @@ class AnagramKeySerializer < ActiveModel::Serializer
   attributes :anagrams
 
   def anagrams
-    all_anagrams = self.object.words.map do |word|
-      word.spelling
-    end
+    all_anagrams = get_word_spelling
     all_anagrams.delete(instance_options[:input_word])
     all_anagrams.sort
+  end
+
+  private
+
+  def get_word_spelling
+    self.object.words.map do |word|
+      word.spelling
+    end
   end
 end
