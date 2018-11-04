f8459cdae69355e958a313614ceb1de3b97b00d1
JP Lynch <JPLynch35@gmail.com>
2018-11-04 08:43:28 -0700

    Adds logic for optional limit to get anagrams response

--- app/serializers/anagram_key_serializer.rb
+++ app/serializers/anagram_key_serializer.rb
@@ -2,16 +2,25 @@ class AnagramKeySerializer < ActiveModel::Serializer
   attributes :anagrams
 
   def anagrams
-    all_anagrams = get_word_spelling
+    all_anagrams = get_word_spellings
     all_anagrams.delete(instance_options[:input_word])
-    all_anagrams.sort
+    reduce_to_limit(all_anagrams.sort)
   end
 
   private
 
-  def get_word_spelling
+  def get_word_spellings
     self.object.words.map do |word|
       word.spelling
+    end
+  end
+
+  def reduce_to_limit(anagrams)
+    if instance_options[:limit]
+      limit = instance_options[:limit].to_i
+      anagrams.take(limit)
+    else
+      anagrams
     end
   end
 end
