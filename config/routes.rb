eaf692495831385049e8ebbc9108387420f93063
JP Lynch <JPLynch35@gmail.com>
2018-11-04 10:16:54 -0700

    Add route to remove all words and anagram keys

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :words, only: [:create]
   delete 'words/:word', to: 'words#destroy'
+  delete 'words', to: 'anagrams#destroy'
   get 'anagrams/:word', to: 'anagrams#show'
 end
