e6a9deb75e7510deecbeb728a5cb9e831c0e637a
JP Lynch <JPLynch35@gmail.com>
2018-11-04 10:08:49 -0700

    Add request test for delete all words

---
+++ spec/requests/delete_all_words_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+describe 'DELETE All Words API' do
+  describe 'DELETE /words.json' do
+    it 'Deletes all words and anagram keys from the data store' do
+      ana = AnagramCreator.new(['dog', 'tile', 'floor', 'lite'])
+      ana.log_anagrams
+
+      expect(Word.count).to eq(4)
+      expect(AnagramKey.count).to eq(3)
+
+      delete '/words.json'
+
+      expect(response.status).to eq(204)
+      expect(Word.count).to eq(0)
+      expect(AnagramKey.count).to eq(0)
+    end
+  end
+end
