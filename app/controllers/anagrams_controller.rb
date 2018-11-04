dcf6574d00e1b20436a0fd01ce26ada98928326a
JP Lynch <JPLynch35@gmail.com>
2018-11-04 08:18:08 -0700

    Refactor anagrams/words controller to anagrams controller

--- app/controllers/anagrams_controller.rb
+++ app/controllers/anagrams_controller.rb
@@ -1,4 +1,4 @@
-class Anagrams::WordsController < ApplicationController
+class AnagramsController < ApplicationController
   def show
     render status: 200, json: AnagramKey.find_by_sorted_spelling(sorted_letters), input_word: params[:word]
   end
