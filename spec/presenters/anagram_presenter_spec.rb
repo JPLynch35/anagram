a8da4d8f6a52ac53e65d1af433036aa8bdfc479b
JP Lynch <JPLynch35@gmail.com>
2018-11-06 15:24:57 -0700

    Create presenter to alter word objects into expected output anagram hash

---
+++ spec/presenters/anagram_presenter_spec.rb
@@ -0,0 +1,33 @@
+require 'rails_helper'
+
+describe AnagramPresenter do
+  it 'exists' do
+    anagram_presenter = AnagramPresenter.new('tank')
+
+    expect(anagram_presenter).to be_an(AnagramPresenter)
+  end
+
+  it 'can reformat output to a hash with an anagrams collection' do
+    anagrams = AnagramCreator.new(['dog', 'tile', 'floor', 'lite'])
+    anagrams.log_anagrams
+    presenter = AnagramPresenter.new('lite')
+  
+    expected = {
+      anagrams: ['tile']
+    }
+
+    expect(presenter.display_anagrams).to eq(expected)
+  end
+
+  it 'can display an empty anagrams collection if no matches found' do
+    anagrams = AnagramCreator.new(['dog', 'floor', 'lite'])
+    anagrams.log_anagrams
+    presenter = AnagramPresenter.new('lite')
+  
+    expected = {
+      anagrams: []
+    }
+
+    expect(presenter.display_anagrams).to eq(expected)
+  end
+end
